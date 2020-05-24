70fc0e570a4c04c525057ffaf878c5dc542a190c
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-05-24 20:49:44 +0200

    Initial version

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Parking from './Parking.svelte'
+</script>
+
+<main>
+	<h1>Parkering</h1>
+	<Parking parkingId="51_1"/>
+	<Parking parkingId="29_1"/>
+	<Parking parkingId="31_1"/>
+	<Parking parkingId="66_1"/>
+	<Parking parkingId="46_1"/>
+	<Parking parkingId="477_1"/>
+</main>
---
+++ src/Parking.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    export let parkingId;
+    let freeSpaces = "";
+    let name = "";
+    let spaces = "";
+    onMount(async () => {
+		const res = await fetch(`/api/Parkeringsdata/ParkeringsdataV1.svc/GetParkeringsytaById/a543a460665e4b64889759dbf83515a7/${parkingId}`);
+		let photos = await res.json();
+		console.log("P " + photos);
+		freeSpaces = photos.ParkingSpacesAvailable;
+		name = photos.Name;
+		spaces = photos.ParkingSpaces;
+	});
+</script>
+<p>{name}: {freeSpaces} ({spaces})</p>
