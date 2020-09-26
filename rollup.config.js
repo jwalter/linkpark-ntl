3c42548a9106d183a87b7170e500e46d9d383097
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-09-26 22:50:21 +0200

    Upgrade Svelte, convert to TS, fix "undefined" bug

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,13 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import autoPreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
@@ -16,6 +18,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: autoPreprocess(),
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
@@ -24,7 +27,7 @@ export default {
 				css.write('public/build/bundle.css');
 			}
 		}),
-
+		typescript({ sourceMap: !production }),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Parking from './Parking.svelte'
 </script>
 
