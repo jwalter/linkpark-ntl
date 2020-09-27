02c6a2632882304ffc9bd4e87e9aecd91afd6145
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-09-27 11:15:39 +0200

    Replace Purecss with Tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import Parking from './Parking.svelte'
+  import Parking from "./Parking.svelte";
 </script>
 
 <main>
-	<div class="pure-g">
-	<Parking parkingId="51_1"/>
-	<Parking parkingId="29_1"/>
-	<Parking parkingId="31_1"/>
-	<Parking parkingId="66_1"/>
-	<Parking parkingId="46_1"/>
-	<Parking parkingId="477_1"/>
-	</div>
+  <div class="grid grid-cols-2 md:grid-cols-3">
+    <Parking parkingId="51_1" />
+    <Parking parkingId="29_1" />
+    <Parking parkingId="31_1" />
+    <Parking parkingId="66_1" />
+    <Parking parkingId="46_1" />
+    <Parking parkingId="477_1" />
+  </div>
 </main>
--- src/Parking.svelte
+++ src/Parking.svelte
@@ -1,32 +1,21 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    export let parkingId;
-    let freeSpaces = "";
-    let name = "";
-    let spaces = "";
-    onMount(async () => {
-		const res = await fetch(`/api/Parkeringsdata/ParkeringsdataV1.svc/GetParkeringsytaById/a543a460665e4b64889759dbf83515a7/${parkingId}`);
-		let parkingGarage = await res.json();
-		freeSpaces = parkingGarage.ParkingSpacesAvailable;
-		name = parkingGarage.Name;
-		spaces = parkingGarage.ParkingSpaces;
-	});
+  import { onMount } from "svelte";
+  export let parkingId;
+  let freeSpaces = "";
+  let name = "";
+  let spaces = "";
+  onMount(async () => {
+    const res = await fetch(
+      `/api/Parkeringsdata/ParkeringsdataV1.svc/GetParkeringsytaById/a543a460665e4b64889759dbf83515a7/${parkingId}`
+    );
+    let parkingGarage = await res.json();
+    freeSpaces = parkingGarage.ParkingSpacesAvailable;
+    name = parkingGarage.Name;
+    spaces = parkingGarage.ParkingSpaces;
+  });
 </script>
-<style>
-.l-box {
-	text-align: center;
-}
-.pname {
-	font-size: 20pt;
-	margin-top: 20px;
-}
-.pfree {
-	font-size: 56pt;
-}
 
-</style>
-
-<div class="pure-u-1-2 pure-u-md-1-3 l-box">
-	<div class="l-box pname">{name}</div>
-	<div class="l-box pfree">{freeSpaces ? freeSpaces : "0?"}</div>
+<div class="leading-tight bg-gradient-to-br from-blue-300 to-blue-100 border-blue-200 border-solid border-2 rounded-md m-2">
+  <div class="text-center text-xl space-x-4 mt-4">{name}</div>
+  <div class="text-center text-6xl text-gray-900 -mt-2">{freeSpaces ? freeSpaces : '0?'}</div>
 </div>
